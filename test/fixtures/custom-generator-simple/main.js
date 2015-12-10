dbacd66f0aad8a4d93231833741436ec6b60c660
Simon Boudrias <admin@simonboudrias.com>
2015-12-10 00:07:13 -0800

    Extract test utilities from yeoman-generator

---
+++ test/fixtures/custom-generator-simple/main.js
@@ -0,0 +1,22 @@
+'use strict';
+var generators = require('yeoman-generator');
+
+// Example of a simple generator.
+//
+// A raw function that is executed when this generator is resolved.
+//
+// It takes a list of arguments (usually CLI args) and a Hash of options
+// (CLI options), the context of the function is a `new Generator.Base`
+// object, which means that you can use the API as if you were extending
+// `Base`.
+//
+// It works with simple generator. If you need to do a bit more complex
+// stuff, extend from Generator.Base and defines your generator steps
+// in several methods.
+
+var Generator = module.exports = generators.Base.extend({
+  exec: function () {}
+});
+
+Generator.description = 'And add a custom description by adding a `description` property to your function.';
+Generator.usage = 'Usage can be used to customize the help output';
