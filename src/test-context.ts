10bcc8fc2ee746d822d473d95b712cffeae8e8e5
Marcelo Shima <marceloshima@gmail.com>
2023-02-27 22:04:49 -0300

    runresult-proxy

--- src/test-context.ts
+++ src/test-context.ts
@@ -1,13 +1,32 @@
-import type Generator from 'yeoman-generator';
 import type RunContext from './run-context.js';
+import type RunResult from './run-result.js';
 
 class TestContext {
+  runResult?: RunResult;
   private runContext?: RunContext<any>;
 
   startNewContext(runContext: RunContext<any>) {
     this.runContext?.cleanupTemporaryDir();
     this.runContext = runContext;
+    this.runResult = undefined;
   }
 }
 
-export default new TestContext();
+const testContext = new TestContext();
+
+export default testContext;
+
+const handler2 = {
+  get(_target, prop, receiver) {
+    if (testContext.runResult === undefined) {
+      throw new Error('Last result is missing.');
+    }
+
+    return Reflect.get(testContext.runResult, prop, receiver);
+  },
+};
+
+/**
+ * Provides a proxy for last executed context result.
+ */
+export const result: RunResult = new Proxy({}, handler2);
