b6e848f2cd23c0d44813b67d7bd988682b489c52
Marcelo Shima <marceloshima@gmail.com>
2026-09-07 07:31:42 -0300

    fix: cleanup node:test mocks

--- src/mocha-cleanup.hooks.js
+++ src/mocha-cleanup.hooks.js
@@ -1,7 +1,10 @@
+import { mock } from 'node:test';
+
 import { context } from './index.js';
 
 export const mochaHooks = {
   afterAll() {
+    mock.reset();
     context.startNewContext();
   },
 };
